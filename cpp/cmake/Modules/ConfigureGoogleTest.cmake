f3f3d7fe4ae0eb7315a87ae8b82ac2aa741e403b
afender <afender@nvidia.com>
2020-05-04 12:20:01 -0500

    Fix ConfigureGoogleTest (closes #2)

--- cpp/cmake/Modules/ConfigureGoogleTest.cmake
+++ cpp/cmake/Modules/ConfigureGoogleTest.cmake
@@ -1,7 +1,9 @@
 set(GTEST_ROOT "${CMAKE_BINARY_DIR}/googletest")
 
-set(GTEST_CMAKE_ARGS " -Dgtest_build_samples=ON" 
-                     " -DCMAKE_VERBOSE_MAKEFILE=ON")
+set(GTEST_CMAKE_ARGS "")
+                     #" -Dgtest_build_samples=ON" 
+                     #" -DCMAKE_VERBOSE_MAKEFILE=ON")
+
 if(NOT CMAKE_CXX11_ABI)
     message(STATUS "GTEST: Disabling the GLIBCXX11 ABI")
     list(APPEND GTEST_CMAKE_ARGS " -DCMAKE_C_FLAGS=-D_GLIBCXX_USE_CXX11_ABI=0")
@@ -26,21 +28,23 @@ if(GTEST_CONFIG)
     message(FATAL_ERROR "Configuring GoogleTest failed: " ${GTEST_CONFIG})
 endif(GTEST_CONFIG)
 
-# Parallel builds cause Travis to run out of memory
-unset(PARALLEL_BUILD)
-if($ENV{TRAVIS})
-    if(NOT DEFINED ENV{CMAKE_BUILD_PARALLEL_LEVEL})
-        message(STATUS "Disabling Parallel CMake build on Travis")
-    else()
-        set(PARALLEL_BUILD --parallel)
-        message(STATUS "Using $ENV{CMAKE_BUILD_PARALLEL_LEVEL} build jobs on Travis")
-    endif(NOT DEFINED ENV{CMAKE_BUILD_PARALLEL_LEVEL})
-else()
-    set(PARALLEL_BUILD --parallel)
-    message("STATUS Enabling Parallel CMake build")
-endif($ENV{TRAVIS})
+set(PARALLEL_BUILD -j)
+if($ENV{PARALLEL_LEVEL})
+    set(NUM_JOBS $ENV{PARALLEL_LEVEL})
+    set(PARALLEL_BUILD "${PARALLEL_BUILD}${NUM_JOBS}")
+endif($ENV{PARALLEL_LEVEL})
 
-execute_process(COMMAND ${CMAKE_COMMAND} --build ${PARALLEL_BUILD} ..
+if(${NUM_JOBS})
+    if(${NUM_JOBS} EQUAL 1)
+        message(STATUS "GTEST BUILD: Enabling Sequential CMake build")
+    elseif(${NUM_JOBS} GREATER 1)
+        message(STATUS "GTEST BUILD: Enabling Parallel CMake build with ${NUM_JOBS} jobs")
+    endif(${NUM_JOBS} EQUAL 1)
+else()
+    message(STATUS "GTEST BUILD: Enabling Parallel CMake build with all threads")
+endif(${NUM_JOBS})
+
+execute_process(COMMAND ${CMAKE_COMMAND} --build .. -- ${PARALLEL_BUILD}
                 RESULT_VARIABLE GTEST_BUILD
                 WORKING_DIRECTORY ${GTEST_ROOT}/build)
 
@@ -52,4 +56,3 @@ message(STATUS "GoogleTest installed here: " ${GTEST_ROOT}/install)
 set(GTEST_INCLUDE_DIR "${GTEST_ROOT}/install/include")
 set(GTEST_LIBRARY_DIR "${GTEST_ROOT}/install/lib")
 set(GTEST_FOUND TRUE)
-
